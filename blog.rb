fe7a6850a7a027b5e1a23ef0c288f574b373d6d7
Matthieu Riviere <poldenton@leukensis.org>
2010-08-28 15:52:17 +0000

    Add configurable blog root url

--- blog.rb
+++ blog.rb
@@ -1,16 +1,18 @@
 require 'sinatra'
 require 'haml'
 
+require_relative 'lib/config'
 require_relative 'lib/db-config'
+require_relative 'lib/links'
 
 class Blog < Sinatra::Application 
 
-	get '/' do
+	get "#{$app_root}/" do
 		posts = Post.get_first_10
 		haml :blog_layout, :locals => { :posts => posts }
 	end
 
-	get '/:id' do
+	get "#{$app_root}/:id" do
 		post = Post.get(params[:id])
 		haml :post_layout, :locals => { :post => post, :show_comments => true }
 	end
