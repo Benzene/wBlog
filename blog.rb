4c15c0edb957c8f29759ebaa41c29502e3e0eb60
Matthieu Riviere <poldenton@leukensis.org>
2010-10-08 02:57:22 +0000

    Initial support for adding and deleting posts

--- blog.rb
+++ blog.rb
@@ -9,9 +9,44 @@ class Blog < Sinatra::Application
 
 	set :root, File.dirname(__FILE__)
 
+        before do
+                if @env['rack.session']['groups'] && ( @env['rack.session']['groups']['Admins'] || @env['rack.session']['groups']['BlogAdmins'] ) then
+			@isadmin = true
+		else
+			@isadmin = false
+                end
+        end
+
+
 	get "/" do
 		posts = Post.get_first_10
 		haml :blog_layout, :locals => { :posts => posts }
+	end
+
+	get "/add" do
+		if !@isadmin then
+			redirect '/blog/'
+		else
+			haml :addpost_layout
+		end
+	
+	end
+
+	post "/add" do
+		if @isadmin then
+			puts @env['rack.session']['uid']
+			puts params['title']
+			puts params['content']
+			Post.newpost(@env['rack.session']['uid'],params['title'],params['content'])	
+		end
+		redirect '/blog/'
+	end
+
+	get "/del/:id" do
+		if @isadmin then
+			Post.get(params[:id]).destroy
+		end
+		redirect '/blog/'
 	end
 
 	get "/n/:id" do
