d4633124792a76b7ea68e27086728e837d2dabb6
Matthieu Riviere <poldenton@leukensis.org>
2010-10-08 02:55:44 +0000

    Move database to a more production-ready place

--- lib/db-config.rb
+++ lib/db-config.rb
@@ -3,7 +3,7 @@ require 'dm-validations'
 
 require_relative 'commons'
 
-DataMapper.setup( :default, "sqlite3:///home/wilya/wBlog/blog.db" )
+DataMapper.setup( :default, "sqlite3:///home/www/dbs/blog.db" )
 
 require_relative '../models/post'
 require_relative '../models/comment'
